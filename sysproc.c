7888e7b32f46273698cead0af85cc4b37e0eac34
SorooshHamedi <hamedisoroosh@gmail.com>
2021-11-14 15:35:05 +0330

    part 3 start

--- sysproc.c
+++ sysproc.c
@@ -102,11 +102,20 @@ sys_digitsum(void)
 int
 sys_setprocessparent(void)
 {
+  int pid;
+  if(argint(0, &pid) < 0)
+    return -1;
+  
+  
+
   return 0;
 }
 
 int
 sys_getparentpid(void)
 {
+  struct proc* parent = myproc()->parent;
+  if(parent == 0)
+    return parent->pid;
   return 0;
 }
