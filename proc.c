faf1e9d86415135be05138b4f29fd8dd84348654
ae2079 <aliebrahimi2079@gmail.com>
2022-01-03 22:22:18 +0330

    Move semaphore to proc file

--- proc.c
+++ proc.c
@@ -6,7 +6,6 @@
 #include "x86.h"
 #include "proc.h"
 #include "spinlock.h"
-#include "semaphore.h"
 
 struct semaphore sems[NSEM];
 
@@ -860,3 +859,46 @@ struct proc* index_dequeue_help(int queueIndex, int procIndex)
 {
   return index_dequeue(&schedulingQueues[queueIndex], procIndex);
 }
+
+void
+sem_init(int i, int v)
+{
+  sems[i].max = v;
+  sems[i].locked = 0;
+  //sems[i].run_first = 0;
+  //sems[i].run_last = 0;
+  sems[i].run_num = 0;
+  sems[i].wait_first = 0;
+  sems[i].wait_last = 0;
+  sems[i].wait_num = 0;
+  // add i to name with sprintf
+  // sems[i].name = "semaophre number i"
+}
+
+uint
+sem_acquire(int i)
+{
+  if(sems[i].locked){
+    return 0;
+  }
+  sems[i].run_num += 1;
+  if(sems[i].run_num >= sems[i].max){
+    sems[i].locked = 1;
+  }
+  return 1;
+}
+
+int
+sem_release(int i)
+{
+  if(sems[i].wait_num){
+    sems[i].wait_num -= 1;
+    int proc = sems[i].wait[sems[i].wait_first];
+    sems[i].wait_first = (sems[i].wait_first + 1) % NPIS;
+    return proc;
+  }
+  sems[i].locked = 0;
+  sems[i].run_num -= 1
+  return -1;
+}
+
--- proc.h
+++ proc.h
@@ -10,9 +10,26 @@ struct cpu {
   struct proc *proc;           // The process running on this cpu or null
 };
 
+// semaphore lock
+struct semaphore {
+  uint max;           // max number of processes that can get semaphore simultaneously
+  uint locked;        // Is the lock held?
+  //uint run[NPIS];     // Queue of processes running in semaphore
+  //uint run_first;     // first of running queue
+  //uint run_last;      // end of running queue
+  uint run_num;       // number of processes in running queue
+  int wait[NPIS];    // Queue of processes waiting for semaphore to be released
+  uint wait_first;    // first of waiting queue
+  uint wait_last;     // end of waiting queue
+  uint wait_num;      // number of processes in waiting queue
+
+  // For debugging:
+  char *name;         // Name of semaphore.
+};
+
 extern struct cpu cpus[NCPU];
 extern int ncpu;
-extern struct semaphore sems[NSEM];
+//extern struct semaphore sems[NSEM];
 
 //PAGEBREAK: 17
 // Saved registers for kernel context switches.
